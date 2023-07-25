53e863c2750957df4879f588df1fe365ce98712a
Hexiang Qi <qihexiang@outlook.com>
2023-07-25 09:36:09 +0800

    Memory record with zod type check

--- router/api/poster.js
+++ router/api/poster.js
@@ -1,12 +1,32 @@
 import { Router } from "express";
 import bodyParser from "body-parser";
+import z from "zod";
+
+const posterSchema = z.object({
+  email: z.string("Email must be a string").email("Email invalid"),
+  content: z
+    .string("Poster content must be a string")
+    .min(5, "Poster too short, must longer than 5 characters")
+    .max(255, "Poster too long, must shorter than 255 characters"),
+});
 
 const posterRouter = Router();
 const jsonParser = bodyParser.json();
 
+let posters = [];
+
 posterRouter.post("/", jsonParser, (req, res) => {
-  console.log(JSON.stringify(req.body));
-  res.send("Received");
+  const validateResult = posterSchema.safeParse(req.body);
+  if (validateResult.success) {
+    posters = [...posters, validateResult.data];
+    res.send("received");
+  } else {
+    res.status(400).send(validateResult.error.message);
+  }
 });
+
+posterRouter.get("/", (req, res) => {
+    res.json(posters)
+})
 
 export default posterRouter;
