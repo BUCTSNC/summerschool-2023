0a140516539a81f843350bb25eceb0a9ccb04e15
Hexiang Qi <qihexiang@outlook.com>
2023-07-26 19:51:47 +0800

    前端示例

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import compression from "compression";
 import express from "express";
+import {join, dirname} from "node:path"
 import timer from "./middlewares/timer.js";
 import apiRouter from "./router/api.js";
 
 const app = express();
 const compressor = compression();
 
+console.log(join(dirname(import.meta.url), "public"))
+
 app.use(compressor);
 app.use(timer);
+const url = new URL(import.meta.url);
+app.use(express.static(join(dirname(url.pathname), "public")));
 app.use("/api", apiRouter);
 app.listen(3000);
