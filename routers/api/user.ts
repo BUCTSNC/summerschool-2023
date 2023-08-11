06b21bee0b386db423b7f1d96e3894df7e327d48
Hexiang Qi <qihexiang@outlook.com>
2023-08-11 15:49:04 +0800

    完成了用户密码的加密和用户资料加载

--- routers/api/user.ts
+++ routers/api/user.ts
@@ -1,35 +1,9 @@
-import { Router } from "express";
 import bodyParser from "body-parser";
-import z, { string } from "zod";
-import prisma from "../../prisma.js";
+import { Router } from "express";
+import z from "zod";
 import sess from "../../middleware/session.js";
-//获取时间函数
-function getUserLoginTime() {
-  var today = new Date();
-  // var dd = today.getDate();
-  // var mm = today.getMonth() + 1;
-  // var yyyy = today.getFullYear();
-  // var hour = today.getHours();
-  // var minutes = today.getMinutes();
-  // var seconds = today.getSeconds();
-  // let todaytime = yyyy + "-" + mm + "-" + dd //+ " " + hour + ":" + minutes + ":" + seconds;
-
-  //console.log(today);
-  return today;
-}
-
-//定义全局变量记录用户登录时间
-let userLoginTimeList: any[] = [];
-
-// {
-//     "email":"3401731883@qq.com",
-//     "password":"Zyd4399"
-//   }管理员账号1
-
-// {
-//     "email":"18539765046@qq.com",
-//     "password":"Zyd4399"
-//   }用户2
+import prisma from "../../prisma.js";
+import bcrypt from "bcryptjs"
 
 const router = Router();
 
@@ -60,7 +34,7 @@ router.post("/registry", bodyParser.json(), async (req, res) => {
           data: {
             email,
             telephone,
-            password,
+            password: await bcrypt.hash(password, 8),
             nickname,
             avatarUrl,
             isAdmin: true,
@@ -103,7 +77,6 @@ router.post("/login", bodyParser.json(), sess, async (req, res) => {
   const { email, password } = result.data;
 
   const user = await prisma.user.findUnique({
-
     where: {
       email
     },
@@ -117,13 +90,9 @@ router.post("/login", bodyParser.json(), sess, async (req, res) => {
     res.status(403).send("用户名或密码不正确")
     return undefined
   }
-  if (password === user.password) {
+  if (await bcrypt.compare(password, user.password)) {
     req.session.userId = user.id;
     res.status(200).send("成功登录")
-
-    //记录用户这一次登录的时间
-    // let nowUserLoginTime = getUserLoginTime();
-    // userLoginTimeList = userLoginTimeList.concat(nowUserLoginTime);
     await prisma.user.update({
       where: {
         id: user.id,
@@ -139,7 +108,25 @@ router.post("/login", bodyParser.json(), sess, async (req, res) => {
 })
 //})
 
+// GET /api/user/:userId
+router.get("/:userId", async (req, res) => {
+  const userId = Number(req.params.userId)
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+    select: {
+      nickname: true, email: true, posts: {
+        select: { id: true, title: true }
+      }
+    }
+  })
 
+  if (user === null) {
+    res.status(404).send("用户不存在")
+    return undefined
+  }
+
+  res.json(user)
+})
 
 //Get/api/user/profile
 router.get("/profile", sess, async (req, res) => {
@@ -202,54 +189,67 @@ router.get("/logout", sess, (req, res) => {
 */
 
 const change_password = z.object(
-    {
-        password: z.string().min(3).max(30),   
-    }
+  {
+    password: z.string().min(3).max(30),
+  }
 )
 
-router.put("/password",bodyParser.json(),sess,async (req,res)=>{
-    const userId = req.session.userId;  //当前登录的用户的id号
-    const data = req.body   //相当于输入的json里的东西
+router.put("/password", bodyParser.json(), sess, async (req, res) => {
+  const userId = req.session.userId;  //当前登录的用户的id号
+  const data = req.body   //相当于输入的json里的东西
 
-    if (userId === undefined) {
-        res.status(403).send("需要登录")
-        return undefined
-    }
-    const result = change_password.safeParse(data)    //保证密码安全性？还得再问问
-    if (result.success) {
-        const { password } = result.data;     //???啥来着
-        try {
-            const created = await prisma.user.update({   //更新内容
-                where: {
-                    id:userId
-                },
-                data: {
-                    password:password
-                }
-            })
-            // posts = [...posts, data]
-            //res.send("Successful")
-            res.send(String(created.id))
-        } catch (err) {
-            console.log()
-            res.status(403).send("无效用户id")
-        }
-    }
-    const id = Number(req.query["id"]);
-    const count = await prisma.post.count({  //post在这里是对的嘛？
-        where: { id, postedBy: { id: userId } }
+  if (userId === undefined) {
+    res.status(403).send("需要登录")
+    return undefined
+  }
+
+
+  const result = change_password.safeParse(data)
+
+  if (!result.success) {
+    res.status(400).send("无效的用户输入")
+    return undefined
+  }
+
+  const { password } = result.data;
+
+  const anotherUserId = req.query.userId;
+
+  if (anotherUserId !== undefined) {
+    const user = await prisma.user.findUnique({
+      where: { id: userId }, select: {
+        isAdmin: true
+      }
     })
-
-    if (count === 1) {
-        await prisma.post.update({
-        where: { id },
-        data: {
-            deletedAt: new Date()
+    if (user?.isAdmin) {
+      const targetUserId = Number(anotherUserId);
+      const updated = await prisma.user.update({
+        where: { id: targetUserId }, data: {
+          password: await bcrypt.hash(password, 8)
         }
-    })
-    res.send("删除成功")
+      })
+      res.send(String(updated.id))
+      return undefined
     }
 
+    res.status(403).send("无权限")
+    return undefined
+  }
+
+  try {
+    const updated = await prisma.user.update({   //更新内容
+      where: {
+        id: userId
+      },
+      data: {
+        password: await bcrypt.hash(password, 8)
+      }
+    })
+    res.send(String(updated.id))
+  } catch (err) {
+    console.log()
+    res.status(403).send("无效的用户ID")
+  }
 })
 
 export default router
